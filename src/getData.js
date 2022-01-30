d5aad6adbc3d6c27c1e01359661a4cdb6eeeeeca
garlife <garlife@gmail.com>
2022-01-30 19:22:21 +0300

    2nd way of slicing

--- src/getData.js
+++ src/getData.js
@@ -1,5 +1,8 @@
 
 export default function getData(){
-  return(fetch('https://jsonplaceholder.typicode.com/posts'))
-  .then((response) => response.json()) //response.<метод чтения>() метод возвращает (читает) ответ в требуемом формате . Без этого метода нельзя будет получить данные через метод fetch()
+  return(
+    fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())//response.<метод чтения>() метод возвращает (читает) ответ в требуемом формате . Без этого метода нельзя будет получить данные через метод fetch()
+  .then((json) => json.slice(0,3)) //.slice(from, to) в моменте получения ответа от сервера
+  )
 }
