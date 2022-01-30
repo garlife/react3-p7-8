5d66b8f83697f22344fd2d1521207f0ff2f6e822
garlife <garlife@gmail.com>
2022-01-30 17:26:57 +0300

    +comment abount fetch().responce

--- src/getData.js
+++ src/getData.js
@@ -1,5 +1,5 @@
 
 export default function getData(){
   return(fetch('https://jsonplaceholder.typicode.com/posts'))
-  .then((response) => response.json())
+  .then((response) => response.json()) //response.{формат данных} метод возвращает ответ в требуемом формате. Без этого метода нельзя будет получить данные через метод fetch()
 }
